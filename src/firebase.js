c234e1074898575cc97edf6cb8d9faf29e422de9
Mohammad Feroz <mohammedferoz.fffs@gmail.com>
2025-12-07 13:06:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBwqwoNH1her5aqm9598BXs1FKpvHcC6E"
-  authDomain: "docfusionbackend.firebaseapp.com",
-  projectId: "docfusionbackend",
-  storageBucket: "docfusionbackend.firebasestorage.app",
-  messagingSenderId: "905176858568",
-  appId: "1:905176858568:web:3a726e219366f4493d0afd"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
